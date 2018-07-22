5dddd5b0f149d45646b5b1dfe3ecd77997912979
Jakub Stefan <kejkun1992@gmail.com>
2018-07-22 18:09:46 +0200

    conversion functions

--- unit_conversions/main.py
+++ unit_conversions/main.py
@@ -11,8 +11,54 @@ Config.set('graphics', 'fullscreen', '0')
 
 
 class UnitConversions(GridLayout):
-    pass
-                
+    def in_to_cm(self, number):
+        try:
+            self.display.text = str(2.54 * float(number))
+        except ValueError:
+            self.display.text = 'Error'
+            
+    def cm_to_in(self, number):
+        try:
+            self.display.text = str(0.393700787 * float(number))
+        except ValueError:
+            self.display.text = 'Error'
+            
+    def ft_to_m(self, number):
+        try:
+            self.display.text = str(0.3048 * float(number))
+        except ValueError:
+            self.display.text = 'Error'
+            
+    def m_to_ft(self, number):
+        try:
+            self.display.text = str(3.2808399 * float(number))
+        except ValueError:
+            self.display.text = 'Error'
+            
+    def yd_to_m(self, number):
+        try:
+            self.display.text = str(0.9144 * float(number))
+        except ValueError:
+            self.display.text = 'Error'
+            
+    def m_to_yd(self, number):
+        try:
+            self.display.text = str(1.0936133 * float(number))
+        except ValueError:
+            self.display.text = 'Error'
+            
+    def kmh_to_ms(self, number):
+        try:
+            self.display.text = str(0.277777778 * float(number))
+        except ValueError:
+            self.display.text = 'Error'
+            
+    def ms_to_kmh(self, number):
+        try:
+            self.display.text = str(3.6 * float(number))
+        except ValueError:
+            self.display.text = 'Error'
+            
        
 class MyApp(App):
     def build(self):
